736ad89d28e495aeaf80b6f72c2ebed865911350
Terrance <notterrancehuang@gmail.com>
2022-05-16 14:56:42 -0400

    get user's top played tracks

--- Main.py
+++ Main.py
@@ -1,9 +1,15 @@
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+import os
+from spotipy.oauth2 import SpotifyOAuth
 
 cid = open("ClientID", 'r')
 c_secret = open("ClientSecret", 'r')
+redirect_url = open("RedirectURL", 'r')
 # Authentication without user. General stats of Spotify
-client_credentials_manager = SpotifyClientCredentials(client_id=cid,
-                                                      client_secret=c_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+scope = "user-top-read"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cid,
+                                               client_secret=c_secret,
+                                               redirect_uri=redirect_url,
+                                               scope=scope))
+
+results = sp.current_user_recently_played()
