f4b36ef4c09de0d4915f6159258271b5afae208d
Terrance <notterrancehuang@gmail.com>
2022-05-24 16:31:41 -0400

    fixed get_top_plays()

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 import requests
+import json
 
 SPOTIFY_GET_TOP_TRACKS_URL = "https://api.spotify.com/v1/me/top/tracks"
 SPOTIFY_CREATE_PLAYLIST_URL = "https://api.spotify.com/v1/users/br7u4dvozt4civyc5wyxdl5x6/playlists"
-ACCESS_TOKEN = "BQBPI-SnMwtSA_2PDMaCbP58u8pOvJga7chFE5wJRg7F5bymgCJ3uuvoh-AxJT6CHJi6JcI3xb7P-erZCYnGPUn1imrdIx2rYHaY_TrAgxnkrc3uQow2pyuD1kAzQMyoDCxIEYQMyX7PyKD9cxlCaZoafKLOZoi4YDO4k4iHX57lobKinkvSm6_RLAhmFh8zzFJPunBM"
+ACCESS_TOKEN = "BQAS321r3F5_b51-qqP3-1SqMUdgKNNYrujNrrtHRk8KDGx92K-bJHv7HLSl6z3-epbjruZ_xZ0qB-6RNU8QRLu8hE0B7n9b9ltiOr3U2SNJkJo1jUd96XwDCbfEXEvH8vb7WR5LvGLlwIpBBvkkyaVP3JFXK1KtNGJ2JrW8jJXxOWA14FJUN0H3F7aoCQJAr1tqL4MugHxzUmyfv_fUlqyHtYMu4JCinFgpGw"
 cid = open("ClientID", 'r')
 c_secret = open("ClientSecret", 'r')
 redirect_url = open("RedirectURL", 'r')
@@ -10,6 +11,7 @@ scope = "user-top-read playlist-modify-public"
 
 
 def request_time_range():
+    # what is the time frame that the top tracks are computed
     time_range_input = input("What is the time range?\n"
                              "\tShort-term is 4 weeks\n"
                              "\tMedium-term is 6 months\n"
@@ -31,8 +33,9 @@ def get_top_plays(limit, time_range):
         headers={
             "Authorization": f"Bearer {ACCESS_TOKEN}"
         },
-        json={
+        params={
             "limit": limit,
+            "offset": 0,
             "time_range": time_range
         }
     )
@@ -58,16 +61,20 @@ def create_playlist(name, public):
 
 
 def main():
-    # limit = input("How many items to return? ")
-    # time_range = request_time_range()
-    # top_plays = get_top_plays(limit, time_range)
-    # print(f"Top plays: {top_plays}")
+    limit = int(input("How many items to return? "))
+    # 0 <= limit <= 50
+    if limit < 0 or limit > 50:
+        limit = 20
+    time_range = request_time_range()
 
-    playlist = create_playlist(
-        name="New test playlist",
-        public=True
-    )
-    print(f"Playlist: {playlist}")
+    top_plays = get_top_plays(limit, time_range)
+    print(f"Top plays: {top_plays}")
+
+    # playlist = create_playlist(
+    #     name="New test playlist",
+    #     public=True
+    # )
+    # print(f"Playlist: {playlist}")
 
 
 if __name__ == "__main__":
