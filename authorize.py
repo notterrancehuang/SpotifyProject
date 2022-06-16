205c2fe3712477278362888ccaa954bf108645ef
Terrance <notterrancehuang@gmail.com>
2022-06-16 14:34:24 -0400

    add get code challenge function

--- authorize.py
+++ authorize.py
@@ -5,7 +5,7 @@ import requests
 import os
 import info
 
-def authorize_user():
+def get_code_challenge():
     code_verifier = base64.urlsafe_b64encode(os.urandom(40)).decode('utf-8')
     code_verifier = re.sub('[^a-zA-Z0-9]+', '', code_verifier)
 
@@ -13,13 +13,17 @@ def authorize_user():
     code_challenge = base64.urlsafe_b64encode(code_challenge).decode('utf-8')
     code_challenge = code_challenge.replace("=", '')
 
+    return code_challenge
+
+
+def authorize_user():
     payload = {
         "response_type": "code",
         "client_id": info.CLIENT_ID,
         "scope": info.scope,
         "redirect_uri": info.redirect_url,
         "code_challenge_method": "S256",
-        "code_challenge": code_challenge,
+        "code_challenge": get_code_challenge(),
     }
 
     response = requests.get(
